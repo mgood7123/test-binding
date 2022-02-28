e2c8e2de6298f542636237d355a2a324d131be45
smallville7123 <smallville7123@gmail.com>
2022-03-01 03:57:04 +1000

    m

---
+++ m/BindingLabel.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace m
+{
+    [XamlCompilation (XamlCompilationOptions.Compile)]
+    public partial class BindingLabel : ContentView
+    {
+        public static readonly BindableProperty TestProperty =
+  BindableProperty.Create("Test", typeof(string), typeof(BindingLabel), "Default");
+
+        public string Test
+        {
+            get { return (string)GetValue(TestProperty); }
+            set {
+                SetValue(TestProperty, value);
+                label.Text = value;
+            }
+        }
+
+        Label label;
+        public BindingLabel()
+        {
+            InitializeComponent();
+            label = new Label() { Text = Test };
+            Content = label;
+        }
+    }
+}
